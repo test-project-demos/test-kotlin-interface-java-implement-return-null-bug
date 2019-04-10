404e63b33087b56080e25e3ab31d77281aad4d17
Cnfn <cnfnss@gmail.com>
2019-04-10 22:18:18 +0800

    init

---
+++ src/main/kotlin/com/example/testkotlininterfacejavaimplementreturnnullbug/TestKotlinInterfaceJavaImplementReturnNullBugApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testkotlininterfacejavaimplementreturnnullbug
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestKotlinInterfaceJavaImplementReturnNullBugApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestKotlinInterfaceJavaImplementReturnNullBugApplication>(*args)
+}
